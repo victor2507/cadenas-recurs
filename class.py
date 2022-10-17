37d5d51f6665c02dd529b2317df40e1eebee1e23
victor2507 <115999899+victor2507@users.noreply.github.com>
2022-10-17 15:26:35 +0200

    Update class.py

--- class.py
+++ class.py
@@ -1,4 +1,15 @@
 class Cadena():
+   def __init__(self,nbr_joueurs):
+      self.nbr_joueurs=nbr_joueurs;
+      
+      
+   def  nbr_candenas(self):
+      nbr_candenas=self.nbr_joueurs;
+      return nbr_candenas;
+     
+      
+   
+   
    
 
    
